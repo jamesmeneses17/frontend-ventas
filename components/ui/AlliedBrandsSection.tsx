ee5820f43d2be5402c81d22238b7a7323181c3d2
James Meneses <124942205+jamesmeneses17@users.noreply.github.com>
2025-11-10 08:24:05 -0500

    seccion 1

--- components/ui/AlliedBrandsSection.tsx
+++ components/ui/AlliedBrandsSection.tsx
@@ -14,6 +14,8 @@ const brands = [
     { name: 'Marca 4', src: '/images/marca4.png' },
       { name: 'Marca 4', src: '/images/marca1.png' },
             { name: 'Marca 4', src: '/images/marca1.png' },
+              { name: 'Marca 6', src: '/images/marca3.png' },
+
 
 
 
@@ -30,7 +32,7 @@ const AlliedBrandsSection: React.FC = () => {
           {brands.map((brand) => (
             <div
               key={brand.name}
-              className="flex-1 flex justify-center items-center min-w-0 max-w-[110px] sm:max-w-[140px] h-8 sm:h-12 relative px-2"
+              className="flex-1 flex justify-center items-center min-w-0 max-w-[160px] sm:max-w-[190px] lg:max-w-[240px] h-16 sm:h-14 lg:h-18 relative px-2"
             >
               {/* Next/Image con fill y objectFit contain para mantener proporción dentro del área fija */}
               <Image
